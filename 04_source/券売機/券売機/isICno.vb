0c79fdded4d7713d1269961540f87c65dd903d49
Abe <hdc242301@gmail.com>
2025-12-22 12:32:14 +0900

    券売機作業中

--- 04_source/券売機/券売機/isICno.vb
+++ 04_source/券売機/券売機/isICno.vb
@@ -1,3 +1,5 @@
 ﻿Public Class isICno
-
+    Private Sub Nbtn_Click(sender As Object, e As EventArgs) Handles Nbtn.Click
+        isICt.Show()
+    End Sub
 End Class
--- 04_source/券売機/券売機/mainf.vb
+++ 04_source/券売機/券売機/mainf.vb
@@ -1,3 +1,17 @@
 ﻿Public Class mainf
+    Private Sub bticket_Click(sender As Object, e As EventArgs) Handles bticket.Click
+        ticketCo.Show()
+    End Sub
 
+    Private Sub bpass_Click(sender As Object, e As EventArgs) Handles bpass.Click
+        pass.Show()
+    End Sub
+
+    Private Sub bictouch_Click(sender As Object, e As EventArgs) Handles bictouch.Click
+        IC.Show()
+    End Sub
+
+    Private Sub bicissue_Click(sender As Object, e As EventArgs) Handles bicissue.Click
+        isICno.Show()
+    End Sub
 End Class
